d441250911b993f05f4fbbc85fe70807aaf09094
sridattaga <sivag.soc95@gmail.com>
2026-03-01 22:25:57 +0530

    Create EcommerceMicroservicesApplication.java

---
+++ common-lib/src/main/java/com/hoangtien2k3/EcommerceMicroservicesApplication.java
@@ -0,0 +1,8 @@
+package com.hoangtien2k3;
+
+@SpringBootApplication
+public class EcommerceMicroservicesApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceMicroservicesApplication.class, args);
+    }
+}
